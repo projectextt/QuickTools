4ce5d7aa71a9e46684913a27dfbd40e0cfba8c31
projectextt <projectextt@gmail.com>
2026-05-13 18:36:22 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Quick Tools",
     "author": "projectextt",
-    "version": (4, 0, 9),
+    "version": (4, 0, 12),
     "blender": (4, 2, 0),
     "location": "View3D > Sidebar > QuickTools",
     "description": "Kumpulan tools animasi dalam satu tab",
